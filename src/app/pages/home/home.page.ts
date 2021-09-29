98345328ecd24f983f8251d9cea0060423c5a76c
TomasEspinoza98 <tomasceatoleii1998@gmail.com>
2021-09-29 16:25:48 -0300

    Guardando los ultimos cambios antes del cambio de la pagina home

--- src/app/pages/home/home.page.ts
+++ src/app/pages/home/home.page.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { AlertController } from '@ionic/angular';
+import { ActivatedRoute } from '@angular/router';
+
 import { STUDENTS } from '../../data/students.data';
 
 @Component({
@@ -9,10 +12,26 @@ import { STUDENTS } from '../../data/students.data';
 export class HomePage implements OnInit {
 
   students = STUDENTS;
+  username: string;
 
-  constructor() { }
 
-  ngOnInit() {
+  constructor(route: ActivatedRoute, private alertController: AlertController) {
+    route.params.subscribe(({ username, login }) => {
+      this.username = username || 'Usuario temporal'
+      this.welcomeAlert(login);
+    });
   }
 
+  ngOnInit() { }
+
+  private async welcomeAlert(isLogin: boolean) {
+    if (isLogin) {
+      const alert = await this.alertController.create({
+        header: 'Bienvenido',
+        message: this.username,
+        buttons: ['Cerrrar']
+      })
+      alert.present();
+    }
+  }
 }
--- src/app/pages/login/login.page.ts
+++ src/app/pages/login/login.page.ts
@@ -1,4 +1,11 @@
 import { Component, OnInit } from '@angular/core';
+import { AlertController } from '@ionic/angular';
+import { Router } from '@angular/router';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms'
+
+
+import { LoadingController } from '@ionic/angular';
+import { alertController } from '@ionic/core';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +14,50 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginPage implements OnInit {
 
-  constructor() { }
+  form: FormGroup;
+  flag = false;
+
+  constructor(
+    private router: Router,
+    private loadingCtrl: LoadingController,
+    private formBuilder: FormBuilder,
+    public alertController: AlertController) { }
 
   ngOnInit() {
+    this.buildForm();
   }
 
+  changeFlag() {
+    this.flag = !this.flag;
+  };
+
+  async login() {
+    const loading = await this.loadingCtrl.create({
+      message: 'Iniciando'
+    });
+    await loading.present();
+
+    setTimeout(async () => {
+      if (this.form.invalid) {
+        await loading.dismiss();
+        const alert = await this.alertController.create({
+          header: 'Error al iniciar sesion',
+          message: 'Nombre de usuario o contraseña incorrecta',
+          buttons: ['Cerrar']
+        });
+
+        return await alert.present();
+      }
+
+      loading.dismiss();
+      this.router.navigate(['/home', { username: this.form.controls.username.value, login: true }])
+    }, 2000);
+  }
+
+  private buildForm(): void {
+    this.form = this.formBuilder.group({
+      username: ['', [Validators.required]],
+      password: [{ value: '', disabled: false }, [Validators.required]]
+    });
+  }
 }
